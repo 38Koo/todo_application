14d8238fccd84b422e17e404e7cef1b254675a45
宮本航平 <captain.marvel.mk@gmail.com>
2023-07-18 06:17:24 -0700

    topデザイン修正

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Heading } from '@chakra-ui/react';
+import { Box, Button, Heading, Text } from '@chakra-ui/react';
 import { Inter } from 'next/font/google';
 import { useRouter } from 'next/router';
 
@@ -24,7 +24,34 @@ export default function Home() {
 
   return (
     <Box>
-      <Heading as="h2">Let&apos;s manage your Task!</Heading>
+      <Heading as="h2">
+        <Box
+          height={200}
+          width={400}
+          border="solid"
+          borderRadius="50px"
+          padding="20px"
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          background="conic-gradient(#ffa500, #ffd700, #ff6347, #deb887, #ffa500)"
+        >
+          <Box
+            border="solid"
+            borderRadius="50px"
+            height={190}
+            width={380}
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            backgroundColor="white"
+          >
+            <Text position="relative" my={0}>
+              Let&apos;s manage your Task!
+            </Text>
+          </Box>
+        </Box>
+      </Heading>
       <Box alignContent="space-around">
         <Button onClick={() => onSubmit(false)}>Sign in</Button>
         <Button onClick={() => onSubmit(true)}>Log in</Button>
